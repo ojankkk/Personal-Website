d24d3adf3e8347cfd0776976a07ddf4e311e16fe
ojankkk <134017034+ojankkk@users.noreply.github.com>
2025-10-31 16:57:42 +0800

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const fontMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "amorim",
-  description: "amorim",
+  title: "Ojankk",
+  description: "Ojankk",
 };
 
 export default function RootLayout({
