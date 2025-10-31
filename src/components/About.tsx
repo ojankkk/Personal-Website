f3b1ccd1a00cad21cc6d705b2893e009b569c70d
ojankkk <134017034+ojankkk@users.noreply.github.com>
2025-10-31 16:55:57 +0800

    About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,15 +3,12 @@ export default function About() {
 		<section id="about" className="space-y-6">
 			<h3 className="text-sm uppercase tracking-wider font-medium text-zinc-500 dark:text-zinc-300">About</h3>
 			<p className="text-zinc-800 dark:text-zinc-200">
-				Ruben Amorim adalah pelatih sepak bola asal Portugal yang kini menukangi Manchester United.
-				Dikenal dengan filosofi permainan menyerang dan pressing intensitas tinggi, Amorim membawa pendekatan taktik modern
-				yang berakar dari pengalamannya bersama Sporting CP. Ia menekankan kerja sama tim, disiplin posisi,
-				serta pengembangan pemain muda dalam setiap proyeknya.
+				Fauzan Azhima Ardani adalah mahasiswa Teknik Informatika di Sekolah tinggi Teknologi Bontang. Saya Memiliki Hobi
+				yaitu bermain game online, basket dan volly. 
 			</p>
 			<p className="text-zinc-800 dark:text-zinc-200">
-				Saat ini, Amorim fokus membangun kembali identitas permainan Manchester United dengan gaya dinamis,
-				cepat dalam transisi, dan berorientasi pada penguasaan bola. Tujuannya jelas mengembalikan kejayaan klub
-				di panggung domestik maupun Eropa.
+				Saat ini, saya berfokus pada perkuliahan dan pekerjaan saya, yang dimana saya berkerja untuk saya bisa kuliah dan menabung 
+				untuk masa depan saya.
 			</p>
 		</section>
 	);
